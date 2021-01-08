769f310105c9317626a9fb1407bfee88da3317f9
Joel Coffman <joel.coffman@usafa.edu>
2021-01-08 15:53:06 -0700

    Attempt to "fuzz" paths for patches

--- install.sh
+++ install.sh
@@ -27,10 +27,12 @@ for service in $services; do
   sudo apt install --yes $(cat "files/packages/$service")
 
   # patch configuration (if patches exist)
-  if [ -e "files/configuration/patches/$service.patch" ]; then
-    sudo patch --dry-run -d / -p0 -ruN < "files/configuration/patches/$service.patch"
-    sudo patch           -d / -p0 -ruN < "files/configuration/patches/$service.patch"
-  fi
+  for patch in $(ls "files/configuration/patches/$service" | sort); do
+    path=${patch:3}  # strip patch number
+    path=${path//--/\/}  # replace `--` with `/`
+    sudo patch --dry-run -ruN $path < "$patch"
+    sudo patch           -ruN $path < "$patch"
+  done
 
   # perform additional configuration (if it exists)
   if [ -e "files/configuration/$service.sh" ]; then
