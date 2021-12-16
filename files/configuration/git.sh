b28131fdc9c7db60ddacd86e21b61a21f4e2b1fb
Joel Coffman <joel.coffman@usafa.edu>
2021-12-16 14:47:57 -0700

    Ensure shell interactive before prompt

--- files/configuration/git.sh
+++ files/configuration/git.sh
@@ -5,6 +5,12 @@ set -o pipefail
 set -u
 
 
+# ensure that shell is interactive (i.e., user can respond to prompt)
+if [ -z "$PS1" ]; then
+  echo "Non-interactive shell, skipping Git configuration"
+  exit
+fi
+
 # global configuration
 for key in name email; do
   echo -n "Please enter your $key for your Git configuration: "
