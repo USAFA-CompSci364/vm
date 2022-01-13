1b07a9c09c783c25f8a149bac253b067ad0cd083
Joel Coffman <joel.coffman@usafa.edu>
2022-01-13 07:49:29 -0700

    Install Git automatically (#13)

---
+++ files/configuration/git.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+set -u
+
+
+# ensure that shell is interactive (i.e., user can respond to prompt)
+if ! tty --silent; then
+  echo "Non-interactive shell, skipping Git configuration"
+  exit
+fi
+
+# global configuration
+for key in name email; do
+  echo -n "Please enter your $key for your Git configuration: "
+  read value
+
+  git config --global "user.$key" "$value"
+done
