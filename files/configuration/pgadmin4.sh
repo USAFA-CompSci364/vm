1e3c9834ebd637f2eda6dc908b709dd3632ec2ac
Joel Coffman <joel.coffman@usafa.edu>
2023-07-20 11:28:05 -0600

    Install pgAdmin 4

---
+++ files/configuration/pgadmin4.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+set -u
+
+
+# echo commands to terminal
+set -x
+
+
+#
+# pgAdmin 4 documentation: https://www.pgadmin.org/download/pgadmin-4-apt/
+#
+
+# install the public key for the repository
+curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub \
+    | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
+
+# create apt configuration file
+sudo sh -c \
+'echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] \
+     https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) \
+     pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
+
+# install pgAdmin 4
+sudo apt install pgadmin4-desktop
